8a0f08031a2db962a3eb1efd240a4753549bf96e
John Hile <John.Hile@FrontEdgePublishing.com>
2023-12-23 08:05:47 -0500

    Fix db migration using dotenv

--- knexfile.ts
+++ knexfile.ts
@@ -1,5 +1,5 @@
-// For more information about this file see https://dove.feathersjs.com/guides/cli/databases.html
 import { app } from './src/app'
+// For more information about this file see https://dove.feathersjs.com/guides/cli/databases.html
 
 // Load our database connection info from the app configuration
 const config = app.get('postgresql')
