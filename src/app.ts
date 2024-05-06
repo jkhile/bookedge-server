f197a96cd817c61eaf14bf37df264bd054286525
John Hile <John.Hile@FrontEdgePublishing.com>
2024-05-06 10:07:07 -0400

    remove extra serveStatic

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,6 @@ app.configure(configuration(configurationValidator))
 
 // Set up Koa middleware
 app.use(cors())
-app.use(serveStatic(`${app.get('public')}/bookedge}`))
 app.use(serveStatic(app.get('public')))
 app.use(errorHandler())
 app.use(parseAuthentication())
