7dc084448793e41df79a73c523a6f0bed451b01e
John Hile <John.Hile@FrontEdgePublishing.com>
2024-05-23 14:42:31 -0400

    Remove private info from logs

--- src/hooks/log-service-call.ts
+++ src/hooks/log-service-call.ts
@@ -1,8 +1,16 @@
 // src/hooks/log-service-call.ts
 import type { HookContext, NextFunction } from '../declarations'
 import type { Params } from '@feathersjs/feathers'
-import { pick, omit, truncate } from 'lodash'
+import { pick, omit } from 'lodash'
 import { logger } from '../logger'
+
+const removeProperties = [
+  'access_token',
+  'accessToken',
+  'refresh_token',
+  'id_token',
+  'password',
+]
 
 export const logServiceCall = async (
   context: HookContext,
@@ -17,14 +25,16 @@ export const logServiceCall = async (
     simplifiedContext = pick(context, ['method', 'path', 'id'])
     simplifiedContext._type = 'before'
     simplifiedContext.params = simplifyParams(context.params)
-    simplifiedContext.data = truncate(JSON.stringify(context.data), {
-      length: 500,
-    })
-
+    simplifiedContext.data = context.data
     const { method, path, _type } = simplifiedContext
     logger.debug(
       `${_type || 'unknown'} ${method || 'unknown'} ${path || 'unknown'}`,
-      simplifiedContext,
+      {
+        context: stringifyAndRemoveProperties(
+          simplifiedContext,
+          removeProperties,
+        ),
+      },
     )
   }
   await next()
@@ -35,15 +45,38 @@ export const logServiceCall = async (
     simplifiedContext._type = 'after'
     const { method, path, _type } = simplifiedContext
     logger.debug(
-      `${_type || 'unknown'} ${method || 'unknown'} ${path || 'unknown'}`,
-      simplifiedContext,
+      `${_type || 'unknown'} ${method || 'unknown'} ${path || 'unknown'} `,
+      {
+        context: stringifyAndRemoveProperties(
+          simplifiedContext,
+          removeProperties,
+        ),
+      },
     )
   }
 }
 
 function simplifyParams(params: Params): Params {
-  const simplified = omit(params, ['authentication', 'connection', 'headers'])
-  delete simplified.user?.access_token
-  delete simplified.user?.refresh_token
-  return simplified
+  return omit(params, ['authentication', 'connection', 'headers'])
+}
+
+function stringifyAndRemoveProperties(
+  object: object,
+  propNames: string[],
+): string {
+  const jsonString = JSON.stringify(object, undefined, 2)
+  // Create a regular expression pattern to match the properties to remove
+  const propPattern = new RegExp(
+    `"(${propNames.join('|')})":\\s*"?[^"]*"?\\s*(?=,|}|])`,
+    'g',
+  )
+
+  // Replace the matched properties with an empty string
+  const updatedJsonString = jsonString.replace(propPattern, '')
+
+  // Remove any trailing commas left after property removal
+  // eslint-disable-next-line unicorn/prefer-string-replace-all
+  const cleanedJsonString = updatedJsonString.replace(/,(?=\s*[\]}])/g, '')
+  console.log('cleanedJsonString:', cleanedJsonString)
+  return cleanedJsonString
 }
