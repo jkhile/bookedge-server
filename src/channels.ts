dbcefa1b5e238bc7767eeb9be22bddc8c19134e3
John Hile <John.Hile@FrontEdgePublishing.com>
2024-06-05 13:56:15 -0400

    Disable the warning about all channels being published

--- src/channels.ts
+++ src/channels.ts
@@ -3,12 +3,12 @@ import type { RealTimeConnection, Params } from '@feathersjs/feathers'
 import type { AuthenticationResult } from '@feathersjs/authentication'
 import '@feathersjs/transport-commons'
 import type { Application, HookContext } from './declarations'
-import { logger } from './logger'
+// import { logger } from './logger'
 
 export const channels = (app: Application) => {
-  logger.warn(
-    'Publishing all events to all authenticated users. See `channels.ts` and https://dove.feathersjs.com/api/channels.html for more information.',
-  )
+  // logger.warn(
+  //   'Publishing all events to all authenticated users. See `channels.ts` and https://dove.feathersjs.com/api/channels.html for more information.',
+  // )
 
   app.on('connection', (connection: RealTimeConnection) => {
     // On a new real-time connection, add it to the anonymous channel
