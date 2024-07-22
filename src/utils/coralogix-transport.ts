c7a53c02fbacc27a0c71768391de3162a852d6a5
John Hile <John.Hile@FrontEdgePublishing.com>
2024-07-22 08:49:19 -0400

    Try fix to get coralogix info severity working

--- src/utils/coralogix-transport.ts
+++ src/utils/coralogix-transport.ts
@@ -1,5 +1,6 @@
 import Transport from 'winston-transport'
 import { Log, CoralogixLogger, LoggerConfig } from 'coralogix-logger'
+import { Severity as CoralogixSeverity } from 'coralogix-logger'
 
 const coralogixKey = process.env.CORALOGIX_KEY
 
@@ -10,14 +11,12 @@ export class CoralogixTransport extends Transport {
     super(options)
 
     const appHost = process.env.HOST
-    console.log('appHost:', appHost)
     let subsystemName = 'development'
     if (appHost?.includes('staging')) {
       subsystemName = 'staging'
     } else if (appHost?.includes('production')) {
       subsystemName = 'production'
     }
-    console.log('subsystemName:', subsystemName)
     const config = new LoggerConfig({
       applicationName: 'bookedge-server',
       privateKey: coralogixKey,
@@ -28,17 +27,45 @@ export class CoralogixTransport extends Transport {
   }
 
   log(info: any, callback: () => void) {
+    console.log('info:', info)
     // info will have message, level and timestamp properties
     setImmediate(() => {
       this.emit('logged', info)
     })
     const log = new Log({
-      // severity: Severity.info,
-      // text: info.message,
+      severity: getSeverity(info.level),
       text: JSON.stringify(info),
     })
+    console.log('log:', log)
     this.coralogixLogger.addLog(log)
 
     callback()
+  }
+}
+
+// Convert severity string to Coralogix Severity enum
+function getSeverity(severity: string): CoralogixSeverity {
+  switch (severity.toLowerCase()) {
+    case 'debug': {
+      return CoralogixSeverity.debug
+    }
+    case 'verbose': {
+      return CoralogixSeverity.verbose
+    }
+    case 'info': {
+      return CoralogixSeverity.info
+    }
+    case 'warn': {
+      return CoralogixSeverity.warning
+    }
+    case 'error': {
+      return CoralogixSeverity.error
+    }
+    case 'critical': {
+      return CoralogixSeverity.critical
+    }
+    default: {
+      return CoralogixSeverity.info
+    }
   }
 }
