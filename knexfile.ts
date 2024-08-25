2cc7a7a1bd2e66c1c59f7b979c1fe600154bded9
John Hile <John.Hile@FrontEdgePublishing.com>
2024-08-25 09:58:29 -0400

    get migrate working on heroku

--- knexfile.ts
+++ knexfile.ts
@@ -1,7 +1,8 @@
-import { app } from './src/app'
-// For more information about this file see https://dove.feathersjs.com/guides/cli/databases.html
-
-// Load our database connection info from the app configuration
-const config = app.get('postgresql')
-
-module.exports = config
+module.exports = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    extension: 'ts',
+    stub: 'migration.stub',
+  },
+}
