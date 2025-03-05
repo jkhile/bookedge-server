1537fde1dfb63f01b2b9d7a38078786a2c298595
John Hile <John.Hile@FrontEdgePublishing.com>
2025-03-05 08:33:10 -0500

    Enable database connection pooling

--- knexfile.ts
+++ knexfile.ts
@@ -1,6 +1,12 @@
 module.exports = {
   client: 'pg',
   connection: process.env.DATABASE_URL,
+  pool: {
+    min: 2,
+    max: 10,
+    acquireTimeoutMillis: 30000,
+    createTimeoutMillis: 30000,
+  },
   migrations: {
     extension: 'ts',
     stub: 'migration.stub',
