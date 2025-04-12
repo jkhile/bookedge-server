192ee5c6c7b20649b33947badf2454f843cd2d47
John Hile <John.Hile@FrontEdgePublishing.com>
2025-04-12 15:05:19 -0400

    Removes unused hook
    Removes the `attachDriveClient` hook, as it is no longer needed in the current implementation.

--- src/hooks/attach-drive-client.ts
+++
@@ -1,6 +0,0 @@
-// For more information about this file see https://dove.feathersjs.com/guides/cli/hook.html
-import type { HookContext } from '../declarations'
-
-export const attachDriveClient = async (context: HookContext) => {
-  return context
-}
