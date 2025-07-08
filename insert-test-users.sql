24e005495663477cd1e4b0a32c48c5e5c4c35013
John Hile <John.Hile@FrontEdgePublishing.com>
2025-07-08 15:24:47 -0400

    Update test users for better security

--- insert-test-users.sql
+++ insert-test-users.sql
@@ -6,23 +6,46 @@ INSERT INTO users (
   status,
   roles,
   fk_created_by,
-  created_at
+  created_at,
+  name
 ) VALUES (
- 'admin@frontedgepublishing.com',
+ 'admin@test.com',
  '$2a$10$XRuGhmBoJgTT5ab28Dilq.WxPskY7yPI4qsMWrdiLpD.koU4f9URa',
  '112784055456984002551',
  '',
  'active',
  ARRAY['admin'],
  1,
- '2023-01-18T19:44:58-5.00'
+ '2023-01-18T19:44:58-5.00',
+ 'Test Admin'
 ), (
- 'john.hile@frontedgepublishing.com',
+ 'projectmanager@test.com',
  '$2a$10$zCjgq4m30ID5KS8AIFRj1ODTxabcW2XWnrrx2Fia2/HjzMhqX.oyG',
  '116600054352122723340',
  '',
  'active',
  ARRAY['productManager'],
  1,
- '2023-01-18T19:44:58-5.00'
+ '2023-01-18T19:44:58-5.00',
+ 'Test Project Manager'
+), (
+ 'editor@test.com',
+ '$2a$10$zCjgq4m30ID5KS8AIFRj1ODTxabcW2XWnrrx2Fia2/HjzMhqX.oyG',
+ '116600054352122723341',
+ '',
+ 'active',
+ ARRAY['editor'],
+ 1,
+ '2023-01-18T19:44:58-5.00',
+ 'Test Editor'
+), (
+ 'author@test.com',
+ '$2a$10$zCjgq4m30ID5KS8AIFRj1ODTxabcW2XWnrrx2Fia2/HjzMhqX.oyG',
+ '116600054352122723342',
+ '',
+ 'active',
+ ARRAY['author'],
+ 1,
+ '2023-01-18T19:44:58-5.00',
+ 'Test Author'
 )
