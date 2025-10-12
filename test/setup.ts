7bd6827bf825050d8bdc4588f228ff708f93c182
John Hile <John.Hile@FrontEdgePublishing.com>
2025-10-12 09:17:41 -0400

    Fixes: Removes extraneous whitespace
    Removes an unnecessary line break in the test setup file.

--- test/setup.ts
+++ test/setup.ts
@@ -5,8 +5,7 @@ import { afterAll, beforeAll } from 'vitest'
 process.env.NODE_ENV = 'test'
 process.env.HOST = 'localhost'
 process.env.PORT = '8998'
-process.env.DATABASE_URL =
-  'postgres://johnhile:@localhost:5432/bookedge-test'
+process.env.DATABASE_URL = 'postgres://johnhile:@localhost:5432/bookedge-test'
 process.env.FEATHERS_SECRET = 'AI6P8NFOCSW637jKfeo1jVGke57cIEgX'
 process.env.CORALOGIX_LOGGER = 'false'
 
